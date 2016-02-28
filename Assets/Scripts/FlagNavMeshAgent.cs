dafe665c668fbf8e9512b3c3df8d16416c79f0fc
Stephen Niedzielski <stephen@niedzielski.com>
2016-02-27 18:01:42 -0700

    Stop FlagNavMeshAgent when in range of destination

--- Assets/Scripts/FlagNavMeshAgent.cs
+++ Assets/Scripts/FlagNavMeshAgent.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class FlagNavMeshAgent : MonoBehaviour {
   FlagCollider flagCollider;
   NavMeshAgent nav;
+  public float range = 2;
 
   void Awake() {
     flagCollider = GetComponentInChildren<FlagCollider>();
@@ -11,7 +12,21 @@ public class FlagNavMeshAgent : MonoBehaviour {
 
   void Update() {
     if (flagCollider.collided.Count > 0) {
-      nav.SetDestination(flagCollider.collided[0].transform.position);
+      Collider other = flagCollider.collided[0];
+      if (inRange(other)) {
+        nav.Stop();
+      } else {
+        nav.Resume();
+        nav.SetDestination(other.transform.position);
+      }
     }
+  }
+
+  bool inRange(Collider other) {
+    return distance(other) < range;
+  }
+
+  float distance(Collider other) {
+    return Vector3.Distance(transform.position, other.transform.position);
   }
 }
